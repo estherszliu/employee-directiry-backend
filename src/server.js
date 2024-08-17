1ff9e5848677b235f28360aa4dea58f285244c4f
Esther Dennis <148415275+estherszliu@users.noreply.github.com>
2024-08-17 12:38:22 +1000

    create database connect to server

---
+++ src/server.js
@@ -0,0 +1,45 @@
+// Importing Express to start using its code
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+
+// Make an instance of an Express server to start configuring it
+const app = express();
+
+
+
+app.get("/", (request, response, next) => {
+    response.json({
+        message:"hello world!"
+    });
+
+});
+
+app.use((error, request, response, next) => {
+
+    response.status(500).json({
+        message:"Error occured in the server.",
+        error: error.message
+    })
+});
+
+// Return a bunch of useful details from the database connection
+// Dig into each property here:
+// https://mongoosejs.com/doc/api/connection.html
+app.get("/dataseHealth", (request, response) => {
+    let databaseState = mongoose.connection.readyState;
+    let databaseName = mongoose.connection.name;
+    let databaseModels = mongoose.connection.modelNames();
+    let databaseHost = mongoose.connection.host;
+
+    response.json({
+        readyState: databaseState,
+        dbName: databaseName,
+        dbModels: databaseModels,
+        dbHost: databaseHost
+    })
+});
+
+
+module.exports = {
+    app
+}
